72be65557f34aefc1d856287901117de4f35a356
yeungle <75871729+yeungle@users.noreply.github.com>
2025-11-30 20:33:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,19 +25,20 @@ rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-openclash
 
 git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-cp -rf kenzok8-packages/ddnsto package/ddnsto
-cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
-cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
-cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
-cp -rf kenzok8-packages/adguardhome package/adguardhome
-cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
+#cp -rf kenzok8-packages/ddnsto package/ddnsto
+#cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+#cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
+#cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
+#cp -rf kenzok8-packages/adguardhome package/adguardhome
+#cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
+cp -rf kenzok8-packages package
 rm -rf kenzok8-packages
 
-git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
-cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
-rm -rf OpenClash-packages
+#git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
+#cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
+#rm -rf OpenClash-packages
 
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
